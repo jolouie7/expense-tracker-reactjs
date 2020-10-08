f4975bb253d7e6f17f390f0fbb5efc0c4b7db55a
Your_Name <joseph161216@gmail.com>
2020-10-07 19:33:19 -0700

    add active and ghost button

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -4,13 +4,33 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 
 const SubHeader = () => {
+  console.log(window.location.href.includes("/expense-list"));
   return (
     <div>
       <Container className="text-center my-3">
-        <Link to="/expense-list">
-          <Button className="mr-1 px-4 py-2">Expense List</Button>
-        </Link>
-        <Button className="ml-1 px-4 py-2">Expense Graphs</Button>
+        {window.location.href.includes("/expense-list") ? (
+          <div>
+            <Link to="/expense-list">
+              <Button className="mr-1 px-4 py-2">Expense List</Button>
+            </Link>
+            <Link to="/expense-graph">
+              <Button variant="outline-primary" className="ml-1 px-4 py-2">
+                Expense Graphs
+              </Button>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <Link to="/expense-list">
+              <Button variant="outline-primary" className="mr-1 px-4 py-2">
+                Expense List
+              </Button>
+            </Link>
+            <Link to="/expense-graph">
+              <Button className="ml-1 px-4 py-2">Expense Graphs</Button>
+            </Link>
+          </div>
+        )}
       </Container>
     </div>
   );
