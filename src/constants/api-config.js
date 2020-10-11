6b9c02e42f2faf119bf2cd0ba89b41d09ad9d5e2
Your_Name <joseph161216@gmail.com>
2020-10-10 20:37:24 -0700

    update backendhost

--- src/constants/api-config.js
+++ src/constants/api-config.js
@@ -3,8 +3,8 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname === "my-personal-expense-tracker.herokuapp.com") {
-  backendHost = "https://my-personal-expense-tracker.herokuapp.com";
+if (hostname === "my-personal-expense-tracker.herokuapp.com/") {
+  backendHost = "https://my-personal-expense-tracker.herokuapp.com/";
 } else if (/^qa/.test(hostname)) {
   backendHost = `https://api.${hostname}`;
 } else {
