15c0de11716be58524db04a85180fbaf141a7c8e
Your_Name <joseph161216@gmail.com>
2020-10-11 00:29:10 -0700

    update protected route

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -1,30 +1,51 @@
-import React from 'react';
-import {Route, Redirect} from "react-router-dom";
-import { connect } from "react-redux";
+// import React from 'react';
+// import {Route, Redirect} from "react-router-dom";
+// import { connect } from "react-redux";
 
-const ProtectedRoute = ({component: Component, isAuthenticated, ...rest}) => {
-  return (
-    <Route {...rest} render={
-      (props) => {
-        if (isAuthenticated) {
-          return <Component {...props} />
-        } else {
-          return <Redirect to={
-            {
-              pathname: "/",
-              state: {
-                from: props.location
-              }
-            }
-          }/>
-        }
-      }
-    }/>
-  )
-}
+// const ProtectedRoute = ({component: Component, isAuthenticated, ...rest}) => {
+//   return (
+//     <Route {...rest} render={
+//       (props) => {
+//         if (isAuthenticated) {
+//           return <Component {...props} />
+//         } else {
+//           return <Redirect to={
+//             {
+//               pathname: "/",
+//               state: {
+//                 from: props.location
+//               }
+//             }
+//           }/>
+//         }
+//       }
+//     }/>
+//   )
+// }
 
-const mapStateToProps = (state) => ({
-  isAuthenticated: state.authReducer.isAuthenticated,
-});
+// const mapStateToProps = (state) => ({
+//   isAuthenticated: state.authReducer.isAuthenticated,
+// });
 
-export default connect(mapStateToProps)(ProtectedRoute)
+// export default connect(mapStateToProps)(ProtectedRoute)
+
+// The react private route component renders a route component if the user is logged in, otherwise it redirects the user to the /login page.
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      localStorage.getItem("user") ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{ pathname: "/", state: { from: props.location } }}
+        />
+      )
+    }
+  />
+);
+
+export default PrivateRoute;
